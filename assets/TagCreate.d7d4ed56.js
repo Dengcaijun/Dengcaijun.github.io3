6de7704be38389f02e21dccf1f4d8b61552e6d44
dengcaijun <12458885+dengcaijun@user.noreply.gitee.com>
2023-12-10 00:36:24 +0800

    '天天记账'

---
+++ assets/TagCreate.d7d4ed56.js
@@ -0,0 +1,1 @@
+import{B as t,M as a}from"./index.7e39d51c.js";import{T as e}from"./TagForm.a8d015ce.js";import{bE as n,bF as o}from"./vendor.f2e53ff1.js";import"./vant.92c246be.js";const i=n({setup:(r,l)=>()=>o(a,null,{title:()=>"\u65B0\u5EFA\u6807\u7B7E",icon:()=>o(t,null,null),default:()=>o(e,null,null)})});export{i as TagCreate,i as default};
